a0fc81fdea4e55104a1c0bd9cdc62322fbc60736
Devendra Negi <devnegikec@gmail.com>
2015-12-27 11:45:44 +0530

    sync and async

---
+++ syncAsync.js
@@ -0,0 +1,48 @@
+/* Sync */
+//Provider
+function add(x,y){
+    console.log("[SP] processing ", x , " and ", y);
+    var result = x + y;
+    console.log("[SP] returning result");
+    return result;
+}
+
+//Consumer
+function addClient(x,y){
+    console.log("[SC] triggering add");
+    var result = add(x,y);
+    console.log("[SC] result = ", result);
+}
+
+/* Async */
+
+//Provider
+function addAsync(x,y,onResult){
+    console.log("[SP] processing ", x , " and ", y);
+    setTimeout(function(){
+        //if either x or y is (undefined | null | 0) throw error
+        if(!x || !y) {
+            throw new Error("invalid arguments");
+            return onResult(err, null);
+        }
+        var result = x + y;
+        console.log("[SP] returning result");
+        //return result;    
+        if (typeof onResult === 'function')
+            onResult(null, result);
+    },3000);
+    
+}
+
+//Consumer
+//modify the below to handle the error and display a meaniful message
+function addAsyncClient(x,y){
+    console.log("[SC] triggering add");
+    addAsync(x,y, function(err,result){
+        if(err){
+            console.log("error occurd..", err);
+            return;
+        }
+        console.log("[SC] result = ", result);    
+    });    
+}
