a92d844895d6607144287cbd9ebf01b2dfc6ee77
Devendra Negi <devnegikec@gmail.com>
2015-12-27 15:02:47 +0530

    deferred objects

--- promises.js
+++ promises.js
@@ -15,3 +15,25 @@ function AddAsyncUsingPromise(x,y){
     });
     return promise;
 }
+
+function AddLazyAsyncUsingPromise(x,y){
+    console.log("[SP] processing ", x , " and ", y);
+    var promise = new Promise(function(resolve, reject){
+                  
+    setTimeout(function(){
+        if(!x || !y){
+           reject('invalide arguments'));
+        }else{ 
+                var result = x +y;
+                console.log("[SP] returning result");
+                resolve(result);
+        }
+    },3000);
+        
+    });
+    return promise;
+}
+
+Promise.all([AddAsyncUsingPromise(100,200), AddLazyAsyncUsingPromise(200, 400)]).then(function(e){
+    
+});
